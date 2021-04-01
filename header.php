1796943a8febd83ad270ce4a2857d6a247519a5e
UdoVanDerWier <65447259+UdoVanDerWier@users.noreply.github.com>
2021-04-01 14:14:41 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,22 @@
+<link rel="stylesheet" href="header.css">
+
+<header>
+    <h1 class = "Title">WorkSpace</h1>
+    <nav class = "navigation">
+        <ul class = "navigation">
+            <li class = "navigation List">Projects</li> 
+            <li class = "navigation List">Feed</li>
+            <li class = "navigation List">Dashboard</li>
+            <li class = "navigation List">CreateProject</li>
+            <li class = "navigation List">profile</li>
+        </ul>
+    </nav>
+
+    <button class = "LoginButton Login">Login</button>
+    <button class = "LoginButton Join">Join</button>
+
+    <button class = "ProfileButton">img</button>
+
+    <button class = "notlificationButton">icon</button>
+    <button class = "messageButton">icon</button>
+</header>
