ff25de390233b04053d046b60a3afe70209fdb68
MauriceBorrel <70626840+MauriceBorrel@users.noreply.github.com>
2021-04-19 14:45:24 +0200

    File Update
    ik heb nu cookies geactiveerd met behulp van javascript en heb de header nu responsive gemaakt

--- src/index.js
+++ src/index.js
@@ -1,4 +1,40 @@
+const cookieStorage = {
+    getItem: (key) => {
+        const cookies = document.cookie
+            .split(';')
+            .map(cookie => cookie.split('='))
+            .reduce ((acc, [key, value]) => ({...acc, [key.trim()]: value }), {})
+        return cookies[key];
+    },
+    setItem: (key, value) => {
+        document.cookie = `${key}=${value}`;
+    },
+};
+
+const storageType = cookieStorage;
+const consentPropertyName = 'myVizion';
+
+const shouldShowPopup = () => !storageType.getItem(consentPropertyName);
+const saveToStorage = () => storageType.setItem(consentPropertyName, true);
+
+window.onload = () => {
+    const consentPopup = document.getElementById('consent-popup');
+    const acceptBtn = document.getElementById('accept');
+
+    const acceptFn = event => {
+        saveToStorage(storageType); 
+        consentPopup.classList.add('hidden');
+    };
+
+    acceptBtn.addEventListener('click', acceptFn);
+
+    if (shouldShowPopup()){
+        consentPopup.classList.remove('hidden');
+    }
+}
+
 $(document).ready(function(){
+    //navbar dropdown list
     $(".dropdown").click(function(){
         $(".dropdown-list ul").toggleClass("active");
     });
@@ -14,5 +50,11 @@ $(document).ready(function(){
             $(".dropdown-list ul").removeClass("active");
         }
     });
+    //hamburger menu
+    $('.toggle').click(function(){
+        $('.toggle').toggleClass('active')
+        $('nav').toggleClass('active')
+    })
 });
+
 
