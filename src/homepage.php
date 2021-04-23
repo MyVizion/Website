50e6f14acc3b6c69ec6ea02d70f440ce57a53848
MauriceBorrel <70626840+MauriceBorrel@users.noreply.github.com>
2021-04-23 10:58:08 +0200

    speciale overview file gemaakt welke de projecten displayt als ze aangemaakt zijn

--- src/homepage.php
+++ src/homepage.php
@@ -1,18 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="/css/style.css" type="text/css" />
-    <!-- Jquery File -->
-    <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-    <!-- Js File -->
-    <script src="/js/index.js"></script>
-    <!-- Fontawesome -->
-    <script src="https://kit.fontawesome.com/62026585de.js" crossorigin="anonymous"></script>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>myVizion</title>
-</head>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <link rel="stylesheet" href="/css/style.css" type="text/css" />
+        <!-- Jquery File -->
+        <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+        <!-- Js File -->
+        <script src="/js/index.js"></script>
+        <!-- Fontawesome -->
+        <script src="https://kit.fontawesome.com/62026585de.js" crossorigin="anonymous"></script>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="shortcut icon" type="image/png" href="/Images/icon.png"/>
+        <title>myVizion</title>
+    </head>
 <body>
 
     <!--search section-------->
@@ -25,13 +26,13 @@
             <div class="form-group">
                 <!--dropdown menu--------->
                 <div class="dropdown">
-                    <div class="default-option">Preconditions&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
+                    <div class="default-option">Categories&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</div>
                     <!--dropdown list--------->
                     <div class="dropdown-list">
                         <ul>
-                           <li>Categories</li>
-                           <li>Likes</li>
-                           <li>Views</li>
+                           <li class="category">Cleaning</li>
+                           <li>Video</li>
+                           <li>Photography</li>
                         </ul>
                     </div>
                 </div>
@@ -50,27 +51,16 @@
         <div class="projects">
             <!--container------------------------>
             <div class="box-container">
-                <?php
-                for ($i = 0; $i < 12; $i++) {
-                    echo "<div class=\"project-box\">
-                    <p class=\"project-name\">Web Development</p>
-                    <div class=\"bottom-border\">
-                    <image><i class=\"far fa-user-circle\"></i><p class=\"project-creator\">&nbsp;User</p></image>
-                    <p class=\"project-likes\"><i class=\"fas fa-heart\"></i>&nbsp;0</p>
-                    <p class=\"project-views\"><i class=\"far fa-eye\"></i>&nbsp;0</p>
-                    </div>
-                    </div>";
-                }
-                ?>         
-            </div>
+                <?php echo view('/template/overview.php') ?>
+            </div> 
         </div>
-             <!--Cookie popup-------------------------->
+
+        <!--Cookie popup-------------------------->
         <div id="consent-popup" class="hidden">
                 <p>By using this site you agree to our <a href='#'>Terms and conditions</a>.
                     Please Accept these before using our site &#x1F36A;.
                     <button type="button" id="accept" class="accept-btn">Got it!</button>
-                </p>
-                
+                </p>     
         </div>
 </body>
 </html>
