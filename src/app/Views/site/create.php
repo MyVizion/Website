2fe7d3f3f8dc5d819cb2f1846048d70c963d0576
MauriceBorrel <mauriceborrel@outlook.com>
2021-05-05 12:12:13 +0200

    file changes

--- src/app/Views/site/create.php
+++ src/app/Views/site/create.php
@@ -1,13 +1,30 @@
-<?= \Config\Services::validation()->listErrors() ?>
-
-<form action="/site/create" method="post">
-    <?= csrf_field() ?>
-
-    <input type="input" name="title" placeholder="Title" /><br/>
-
-    <textarea name="info" placeholder="type in project info"></textarea><br/>
-
-    <input type="file" name="image" value=""/><br/>
-
-    <input type="submit" name="submit" value="Create Project" />
-</form>
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <link rel="shortcut icon" href="/Images/logos/iconDarkRed.png" type="image/x-icon">
+            <link rel="stylesheet" href="/css/create.css" type="text/css" />
+            <!-- Fontawesome -->
+            <script src="https://kit.fontawesome.com/62026585de.js" crossorigin="anonymous"></script>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>myVizion</title>
+        </head>
+    <body>
+        <div class="parent">
+            <?= \Config\Services::validation()->listErrors() ?>
+                <form action="/site/create" method="post">
+                <h1>Create Project</h1>
+                    <?= csrf_field() ?>
+                    <div class="form-group">
+                        <input class="ph-title" type="input" name="title" placeholder="Title" /><br/>
+                    </div>
+                    <div class="grow-wrap">
+                        <textarea class="ph-info" name="info" placeholder="Type in project info"></textarea><br/>
+                    </div>
+                    <input class="file-btn" type="file" name="image" value="Choose File"/><br/>
+                    <input class="create-btn" type="submit" name="submit" value="Create Project"/>
+                </form>
+        </div>
+    </body>
+</html>
