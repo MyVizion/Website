62b925ec0f9812d69bebd4eed9e644094890e9af
MauriceBorrel <mauriceborrel@outlook.com>
2021-05-11 12:09:31 +0200

    file updates

--- src/app/Views/site/create.php
+++ src/app/Views/site/create.php
@@ -5,8 +5,9 @@
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <link rel="shortcut icon" href="/Images/logos/iconDarkRed.png" type="image/x-icon">
             <link rel="stylesheet" href="/css/create.css" type="text/css" />
-            <!-- Js File -->
-            <script src="/js/file.js"></script>
+         
+            <!-- Jquery File -->
+            <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
             <!-- Fontawesome -->
             <script src="https://kit.fontawesome.com/62026585de.js" crossorigin="anonymous"></script>
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -18,12 +19,19 @@
             <form action="/site/create" method="post" enctype="multipart/form-data">
                  <h1>Create Project</h1>
                     <?= csrf_field() ?>
+                        <!--Title-->
                         <div class="form-group">
                             <input class="ph-title" type="input" name="title" placeholder="Title" /><br/>
                         </div>
+                        <!--Info-->
+                        <div class="form-group">
+                            <input class="ph-title" type="input" name="creator" placeholder="Username" /><br/>
+                        </div>
+                        <!--Username-->
                         <div class="grow-wrap">
                             <textarea class="ph-info" name="info" placeholder="Type in project info"></textarea>
                         </div>
+                        <!--File-->
                         <div class="file-input">
                             <input class="file" id="file" type="file" name="image"/>
                                 <label for="file">
@@ -34,5 +42,7 @@
                 <input class="create-btn" type="submit" name="submit" value="Create Project"/>
             </form>
         </div>
+           <!-- Js File -->
+           <script src="/js/file.js"></script>
     </body>
 </html>                                                                                                                                                                                                                                  
